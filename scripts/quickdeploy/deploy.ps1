3debb85a3ea967bb6a8cc6f8548b8ec70c53fee9
Daniel Scott-Raynsford <plagueho@gmail.com>
2025-05-05 20:49:54 +1200

    Add PowerShell script for Azure AI Foundry Jumpstart deployment automation

---
+++ scripts/quickdeploy/deploy.ps1
@@ -0,0 +1,39 @@
+<#
+    .SYNOPSIS
+        A PowerShell script that combines configuration and deployment of the Azure AI Foundry Jumpstart
+        into a single command.
+
+    .DESCRIPTION
+        This script automates the process of deploying the Azure AI Foundry Jumpstart. It combines the
+        configuration and deployment steps into a single command, making it easier to set up the environment.
+        It just makes calls to the Azure Developer CLI command line tool (azd) to do the work.
+
+    .PARAMETER EnvironmentName
+        The name of the environment to deploy. If not specified, Azure Developer CLI will prompt the user.
+
+    .PARAMETER EnvironmentVariables
+        A hashtable of environment variables to set for the deployment.
+#>
+
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory = $false)]
+    [System.String]
+    $EnvironmentName,
+
+    [Parameter(Mandatory = $false)]
+    [hashtable]
+    $EnvironmentVariables
+)
+
+foreach ($key in $environmentVariables.Keys) {
+    Write-Host "Setting environment variable: $key = $($environmentVariables[$key])"
+    azd env set $key $($environmentVariables[$key])
+}
+
+if ($Environment) {
+    Write-Host "Using environment: $Environment"
+    azd up --environment $EnvironmentName
+} else {
+    azd up
+}
